27e659eab49051cceb4233946c8493f03b4f0ccc
basicula <maksim.basum@gmail.com>
2020-12-27 22:38:28 +0200

    More flexible menu

--- script/menu.js
+++ script/menu.js
@@ -1,15 +1,16 @@
 $(document).ready(function(){
-    $(".dropbtn").click(function(){
+    $(".dropbtn").click(function() {
         var children = $(this).parent().children(".dropdown-content");
         var display = $(children).css("display");
         if (display == "none")
-            $(children).css("display", "block")
+            $(children).css("display", "block");
         else
-            $(children).css("display", "none")
+            $(children).css("display", "none");
     });
-});
-
-$(document).click(function(e) {
-    if (!e.target.matches('.dropbtn'))
-        $(".dropdown-content").css("display", "none");
+    $(".dropbtn").blur(function() {
+        var children = $(this).parent().children(".dropdown-content");
+        var display = $(children).css("display");
+        if (display !== "none")
+            $(children).css("display", "none");
+    });
 });
